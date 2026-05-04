f0ba227f5ca18c84b09cdb6c56a99d3d6cf574b9
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-05-04 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/Controller/IndexControllerFactory.php
+++ src/Service/Controller/IndexControllerFactory.php
@@ -2,7 +2,7 @@
 
 namespace XmlViewer\Service\Controller;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use XmlViewer\Controller\IndexController;
 
